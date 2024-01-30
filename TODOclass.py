2550921383bac9e2a50e82677aed480067de87d1
Pradhu007 <55123592+Pradhu007@users.noreply.github.com>
2024-01-30 21:32:55 +1100

    Create TODOclass.py

---
+++ TODOclass.py
@@ -0,0 +1,11 @@
+class Item:
+    name = ""
+    description = ""
+    time = ""
+    sound = ""
+
+    def __init__(self,name,description,time,sound=""):
+        self.name = name
+        self.description = ""
+        self.time = time
+        self.sound = sound
